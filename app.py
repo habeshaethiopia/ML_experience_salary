a3dc32300732068c9112807c5e8c0b98be84f798
uchuulaa <elijahbag456@gmail.com>
2024-12-22 23:45:10 +0300

    feat: Added Salary vs. Years of Experience insights with graph

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 import GUI
 import streamlit as st
-
-from GUI.index import home, dataset, predict_salary, about_us, graph_analysis
 from streamlit_option_menu import option_menu
+from GUI.index import home, dataset, predict_salary, about_us, graph_analysis, salary_insights
 
-
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     with st.sidebar:
         selected = option_menu(
             menu_title="Main Menu",
-            options=["Home", "Dataset", "Predict Salary","Graph Analysis", "About Us"],
-            icons=["house", "book", "envelope", "bar-chart", "info-circle"],
+            options=["Home", "Dataset", "Predict Salary", "Graph Analysis", "Salary Insights", "About Us"],
+            icons=["house", "book", "envelope", "bar-chart", "clipboard-data", "info-circle"],  # Added icon
             menu_icon="cast",
             default_index=0,
         )
@@ -26,5 +22,7 @@ if __name__=='__main__':
         predict_salary()
     elif selected == 'Graph Analysis':
         graph_analysis()
+    elif selected == 'Salary Insights':  # New section
+        salary_insights()
     elif selected == 'About Us':
         about_us()
